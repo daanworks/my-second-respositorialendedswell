85610a3758646ab618c46f10b2448c6493197b66
daanworks <daanworks@users.noreply.github.com>
2020-04-28 16:05:28 +0200

    asd

---
+++ script.js
@@ -0,0 +1,26 @@
+var miles = 900;
+var kilometers = miles * 1.6;
+
+var boo = true;
+var notboo = false;
+
+var cars = ["Saab", "Volvo", "BMW"]; // Array
+
+//Object:
+
+var person = {
+    name: 'DAANWORKS',
+    age: 30,
+    oldEnoughToDrive: true,
+    favouriteCars: cars
+}
+
+
+
+
+$('#add-button').click(() => {
+    $('h1').text('I can overwrite you with jQuery, you little bitch.');
+});
+
+
+
